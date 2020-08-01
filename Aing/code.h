559507fc639dc3bdd3fcb8643a1b54c5578d6f1d
TreeKR <johanbag290@gmail.com>
2020-08-01 23:16:51 +0900

    add code.h

---
+++ Aing/code.h
@@ -0,0 +1,8 @@
+#define addPointer 1
+#define subPointer 2
+#define addByteAtPointer 3
+#define subByteAtPointer 4
+#define asciiOut 5
+#define asciiIn 6
+#define whilePointerZeroStart 7
+#define whilePointerZeroEnd 8
