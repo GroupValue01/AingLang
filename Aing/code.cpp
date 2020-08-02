0db78fa01e97a9d001c487b4a0f3387a781f6f83
TreeKR <johanbag290@gmail.com>
2020-08-02 21:02:17 +0900

    중간저장

--- Aing/code.cpp
+++ Aing/code.cpp
@@ -1,7 +1,21 @@
 #include "code.h"
 
-char* Code::getCode(wstring charArray[]) {
-    char arr[1000000];
+char* Code::getCode(wstring wstr) {
+    char arr[1000000] = wstr;
 
-    return arr;
+    char spl[INT_MAX];
+
+    int a = 0;
+
+    for(int i = 0; i < sizeof(arr); i++) {
+        if(arr[i] == '잉') {
+            a = 0;
+        }
+        if(arr[i] == '이') {
+            a++;
+            spl[i] = (char)a;
+        }
+    }
+
+    return spl;
 }
--- Aing/code.h
+++ Aing/code.h
@@ -1,15 +1,15 @@
 #include "aing.h"
 // command define
-#define addPointer 1
-#define subPointer 2
-#define addByteAtPointer 3
-#define subByteAtPointer 4
-#define asciiOut 5
-#define asciiIn 6
-#define whilePointerZeroStart 7
-#define whilePointerZeroEnd 8
+#define addPointer '1'
+#define subPointer '2'
+#define addByteAtPointer '3'
+#define subByteAtPointer '4'
+#define asciiOut '5'
+#define asciiIn '6'
+#define whilePointerZeroStart '7'
+#define whilePointerZeroEnd '8'
 
 class Code {
     public:
-        char * getCode(wstring charArray[]);
+        char * getCode(wstring wstr);
 };
